c58376cb3f125f29aa6dfe7785fb7c6f7c07da0b
asaku235 <shimoyama626@gmail.com>
2019-05-11 02:29:53 -0400

    Add controller for only markup

---
+++ app/controllers/infos_controller.rb
@@ -0,0 +1,10 @@
+class InfosController < ApplicationController
+  def index
+  end
+  def new
+  end
+  def edit
+  end
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :users, only: [:show, :edit, :update, :index, :new]
+  resources :users, only: [:show, :edit, :update, :index, :new] do
+    resources :infos
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'items#index'
   resources :items
