cdc1fd173862aed0cd6af55cbc14f33324360a7f
Sora-bs <aozora@icloud.com>
2019-05-13 16:18:25 +0900

    Edit

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -33,35 +33,42 @@ $(document).on('turbolinks:load', function () {
     },
   });
 
-  var myswiper2 = new Swiper('.swiper2', {
-    spaceBetween: 10,
-    slideToClickedSlide: true
-  });
-  var myswiper3 = new Swiper('.swiper3', {
-    centeredSlides: true,
-    paginationClickable: false,
-    simulateTouch: true,
-    slidesPerView: 4,
-    speed: 500,
-    breakpoints: {
-      // 980ピクセル幅以下になったら
-      980: {
-        slidesPerView: 3,
-        spaceBetween: 30
+  $(function () {
+    /*クリックイベント*/
+    // $('.owl-dot').on('mouseover', function () {
+
+    var myswiper2 = new Swiper('.swiper2', {
+      spaceBetween: 10,
+      slideToClickedSlide: true
+    });
+
+    var myswiper3 = new Swiper('.swiper3', {
+      centeredSlides: true,
+      paginationClickable: false,
+      simulateTouch: true,
+      slidesPerView: 4,
+      speed: 500,
+      breakpoints: {
+        // 980ピクセル幅以下になったら
+        980: {
+          slidesPerView: 3,
+          spaceBetween: 30
+        },
+        // 640ピクセル幅以下になったら
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20
+        }
       },
-      // 640ピクセル幅以下になったら
-      640: {
-        slidesPerView: 2,
-        spaceBetween: 20
-      }
-    },
-    controller: {
-      control: myswiper2,
-      inverse: false,
-      by: 'slide',
-    },
-    // spaceBetween: 10,
-    slideToClickedSlide: true
-  })
-  myswiper2.controller.control = myswiper3;
+      controller: {
+        control: myswiper2,
+        inverse: false,
+        by: 'slide',
+      },
+      spaceBetween: 10,
+      slideToClickedSlide: true,
+    })
+    myswiper2.controller.control = myswiper3;
+    // });
+  });
 });
