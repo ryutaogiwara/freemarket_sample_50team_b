3beb7f6f9e641a1c3e3bb4f711144d9bf82079e4
Sora-bs <aozora@icloud.com>
2019-05-16 12:57:42 +0900

    rspec

---
+++ spec/factories/items.rb
@@ -0,0 +1,20 @@
+FactoryBot.define do
+
+  factory :item do
+    name              {"aaa"}
+    description       {"説明内容"}
+    state             {"1"}
+    postage           {"1"}
+    region            {"1"}
+    shipping          {"1"}
+    shipping_data     {"1"}
+    price             {"1000"}
+    category          {"1"}
+    size              {"1"}
+    saler_id          {"1"}
+    transaction_id    {"1"}
+    created_at        {"2019-05-10 09:46:46"}
+    updated_at        {"2019-05-10 09:46:46"}    
+  end
+
+end
