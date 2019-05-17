3d6eaca1aaae821ecf5df46269785ac1bad6da2b
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2019-05-17 19:28:31 +0900

    Update exhibit.js
    ・販売手数料を求める式追加
    ・販売利益を求める式追加
    
    if文で条件分岐して３００円以下で発火しないように調整予定

--- app/assets/javascripts/exhibit.js
+++ app/assets/javascripts/exhibit.js
@@ -1,7 +1,24 @@
 $(document).on('turbolinks:load', function () {
   $(function () {
-    var sale_fee = $("#sale-fee-result");
-    var sale_profit = $("#sale-profit-result");
+    $("#price-input-field").on("keyup", function () {
+      var price = $("#price-input-field").val();
 
-  })
+      var fee = 0.1;
+      var resultfee = price * fee;
+      var resultprofit = price - resultfee;
+
+      price = parseInt(price);
+      fee = parseInt(fee);
+      resultfee = parseInt(resultfee);
+      resultprofit = parseInt(resultprofit);
+
+      $("#sale-fee-result").text(resultfee);
+      $("#sale-profit-result").text(resultprofit);
+
+      // console.log(price);
+
+
+    })
+
+  });
 });
