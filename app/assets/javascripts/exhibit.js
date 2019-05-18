0085302e44981da72e61779179a69fea33395368
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2019-05-18 14:52:25 +0900

    Update exhibit.js
    ・実行条件の追加
    ・カンマ区切り表記へ変更
    ・手数料計算調整

--- app/assets/javascripts/exhibit.js
+++ app/assets/javascripts/exhibit.js
@@ -2,23 +2,25 @@ $(document).on('turbolinks:load', function () {
   $(function () {
     $("#price-input-field").on("keyup", function () {
       var price = $("#price-input-field").val();
-
       var fee = 0.1;
+      // 手数料計 小数点切り下げ
       var resultfee = price * fee;
+      var resultfee = Math.floor(resultfee);
+      // 利益計算
       var resultprofit = price - resultfee;
+      // カンマ区切り表示
 
-      price = parseInt(price);
-      fee = parseInt(fee);
-      resultfee = parseInt(resultfee);
-      resultprofit = parseInt(resultprofit);
-
-      $("#sale-fee-result").text(resultfee);
-      $("#sale-profit-result").text(resultprofit);
-
-      // console.log(price);
-
-
+      // var a = resultfee + "";
+      // var b = resultfee.replace(/(\d)(?=(\d\d\d)+$)/g, "$1,");
+      // 実行条件　￥300〜9999999まで　その他の値はデフォルトのーを返す
+      if (300 <= price && price <= 9999999) {
+        $("#sale-fee-result").text("¥" + resultfee.toLocaleString());
+        $("#sale-profit-result").text("¥" + resultprofit.toLocaleString());
+      }
+      else {
+        $("#sale-fee-result").text("-");
+        $("#sale-profit-result").text("-");
+      }
     })
-
-  });
+  })
 });
