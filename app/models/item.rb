8304ea9d7ce8b902ab3f9b43727a58d64e50c0b4
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2019-05-21 15:11:23 +0900

    Update item.rb
    アソシエーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,9 @@
 class Item < ApplicationRecord
+   extend ActiveHash::Associations::ActiveRecordExtensions
   has_many :transactions, dependent: :destroy
   has_many :images
   accepts_nested_attributes_for :images, allow_destroy: true
   belongs_to :user
-  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
 end
 
