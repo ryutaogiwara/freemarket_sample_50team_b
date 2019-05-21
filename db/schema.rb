5a2cc6bf1de1024bba507b3eb5cc48e8da6c1084
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2019-05-21 15:33:31 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_19_053259) do
+ActiveRecord::Schema.define(version: 2019_05_19_065055) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "zipcord", null: false
@@ -58,6 +58,8 @@ ActiveRecord::Schema.define(version: 2019_05_19_053259) do
     t.string "brand"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -112,4 +114,5 @@ ActiveRecord::Schema.define(version: 2019_05_19_053259) do
   add_foreign_key "addresses", "users"
   add_foreign_key "card_infos", "users"
   add_foreign_key "images", "items"
+  add_foreign_key "items", "users"
 end
