abe7945a14d9e74e2f5fd1f735d2192cb3074421
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2019-05-21 17:16:35 +0900

    Update 20190521075528_change_data_region_to_items.rb
    ロールバックに対応するためup,down表記に変更

--- db/migrate/20190521075528_change_data_region_to_items.rb
+++ db/migrate/20190521075528_change_data_region_to_items.rb
@@ -1,5 +1,8 @@
 class ChangeDataRegionToItems < ActiveRecord::Migration[5.2]
-  def change
+  def up
     change_column :items, :region, :integer
+  end
+  def down
+    change_column :items, :region, :text
   end
 end
