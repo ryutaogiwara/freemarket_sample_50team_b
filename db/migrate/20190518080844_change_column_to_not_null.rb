5e66d19ac08bbfc7df0bbf59eb57768cc6366960
zxp736 <cod736@gmail.com>
2019-05-24 11:36:53 +0900

    deploy修正

--- db/migrate/20190518080844_change_column_to_not_null.rb
+++ db/migrate/20190518080844_change_column_to_not_null.rb
@@ -1,9 +1,9 @@
 class ChangeColumnToNotNull < ActiveRecord::Migration[5.2]
   def up
-    change_column :card_infos, :number, :integer, null: true
-    change_column :card_infos, :month, :integer, null: true
-    change_column :card_infos, :year, :integer, null: true
-    change_column :card_infos, :security_code, :integer, null: true
+    change_column :card_infos, :number, :integer
+    change_column :card_infos, :month, :integer
+    change_column :card_infos, :year, :integer
+    change_column :card_infos, :security_code, :integer
   end
 
   def down
