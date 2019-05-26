677107b60bf7ddb2d9bc61ea9cae4a7202c2445f
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2019-05-26 17:51:07 +0900

    Update listings_controller.rb
    ステータスごとのビュー内容変更設定

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,8 +1,16 @@
 class ListingsController < ApplicationController
     # before_action :require_signin
   def index
-    @items = Item.where(user_id: current_user.id).limit(3).order("created_at DESC")
-
+    @items = Item.where(user_id: current_user.id, trade_status: 1).limit(3).order("created_at DESC")
   end
+
+  def in_progress
+    @items = Item.where(user_id: current_user.id, trade_status: 2).limit(3).order("created_at DESC")
+  end
+
+  def completed
+    @items = Item.where(user_id: current_user.id, trade_status: 3).limit(3).order("created_at DESC")
+  end
+
 end
 
