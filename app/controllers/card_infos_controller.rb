e87ccb6a0a909e064520a38126eab00731229693
t-miyamoto0809 <t.miyamoto0809@gmail.com>
2019-05-27 15:18:02 +0900

    カードのブランドロゴ表示

--- app/controllers/card_infos_controller.rb
+++ app/controllers/card_infos_controller.rb
@@ -9,7 +9,7 @@ class CardInfosController < ApplicationController
   def pay #payjpとCardのデータベース作成を実施します。
     Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
     if params[:payjp_token].blank?
-       redirect_to action: "new"
+      redirect_to action: "new"
     else
       customer = Payjp::Customer.create(card: params[:payjp_token])
       @card = CardInfo.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
@@ -38,6 +38,21 @@ class CardInfosController < ApplicationController
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
       customer = Payjp::Customer.retrieve(@card.customer_id)
       @default_card_information = customer.cards.retrieve(@card.card_id)
+      @card_brand = @default_card_information.brand #カード会社のブランドアイコンを表示
+      case @card_brand
+      when "Visa"
+        @card_src = "visa.svg"
+      when "JCB"
+        @card_src = "jcb.svg"
+      when "MasterCard"
+        @card_src = "master-card.svg"
+      when "American Express"
+        @card_src = "american_express.svg"
+      when "Diners Club"
+        @card_src = "dinersclub.svg"
+      when "Discover"
+        @card_src = "discover.svg"
+      end
     end
   end
 end
