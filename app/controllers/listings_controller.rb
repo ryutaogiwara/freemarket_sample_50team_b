30d7ed271c72c334c932211740c3d9ad85959866
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2019-05-30 04:54:54 +0900

    Update listings_controller.rb
    商品状態進捗管理の検索条件修正

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -5,12 +5,21 @@ class ListingsController < ApplicationController
   end
 
   def in_progress
-    @reserveditem = Item.where(user_id: current_user.id, trade_status: 2).limit(3).order("created_at DESC")
-    @shippeditem = Item.where(user_id: current_user.id, trade_status: 3).limit(3).order("created_at DESC")
+    @reserveditems = Item.where(user_id: current_user.id, trade_status: 2).limit(5).order("created_at DESC")
+    @shippeditems = Item.where(user_id: current_user.id, trade_status: 3).limit(5).order("created_at DESC")
   end
 
   def completed
-    @items = Item.where(user_id: current_user.id, trade_status: 3).limit(3).order("created_at DESC")
+    @items = Item.where(user_id: current_user.id, trade_status: 4).limit(10).order("created_at DESC")
+  end
+
+  def purchase
+    @purchaseitems = Item.where(buyer_id: current_user.id, trade_status: 2).limit(5).order("created_at DESC")
+    @purchaseditems = Item.where(buyer_id: current_user.id, trade_status: 3).limit(5).order("created_at DESC")
+  end
+
+  def purchased
+    @items = Item.where(buyer_id: current_user.id, trade_status: 4).limit(10).order("created_at DESC")
   end
 
 end
