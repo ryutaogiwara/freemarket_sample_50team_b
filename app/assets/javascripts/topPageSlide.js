ecca7762a70ad9939ac2f7dd214dbc5df921f784
Sora-bs <aozora@icloud.com>
2019-05-31 10:19:06 +0900

    EditTopPageSlide

--- app/assets/javascripts/topPageSlide.js
+++ app/assets/javascripts/topPageSlide.js
@@ -21,7 +21,12 @@ $(document).on('turbolinks:load', function () {
       640: {
         slidesPerView: 2,
         spaceBetween: 20
-      }
+      },
+
+      340: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
     },
     pagination: {
       el: '.swiper-pagination',
